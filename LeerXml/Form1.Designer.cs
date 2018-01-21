72669d9507271cd4e47fda6735f84ff92b513762
Markos16 <marcos.ee16@gmail.com>
2018-01-21 13:57:36 -0600

    PRimer Commit

---
+++ LeerXml/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LeerXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.gridParametros = new System.Windows.Forms.DataGridView();
+            this.gridDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridParametros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(385, 12);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // gridParametros
+            // 
+            this.gridParametros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridParametros.Location = new System.Drawing.Point(13, 55);
+            this.gridParametros.Name = "gridParametros";
+            this.gridParametros.Size = new System.Drawing.Size(834, 101);
+            this.gridParametros.TabIndex = 1;
+            // 
+            // gridDatos
+            // 
+            this.gridDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDatos.Location = new System.Drawing.Point(13, 235);
+            this.gridDatos.Name = "gridDatos";
+            this.gridDatos.Size = new System.Drawing.Size(834, 279);
+            this.gridDatos.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 526);
+            this.Controls.Add(this.gridDatos);
+            this.Controls.Add(this.gridParametros);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridParametros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.DataGridView gridParametros;
+        private System.Windows.Forms.DataGridView gridDatos;
+    }
+}
+
---
+++ LeerXml/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace LeerXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Title = "Buscar medición en formato XML";
+            openFileDialog1.DefaultExt = ".xml";
+            openFileDialog1.Filter = "archivos csv (*.csv)|*.csv";
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.ToString() != " ")
+            {
+                
+                if (File.Exists(""+openFileDialog1.FileName))
+                {
+                    string line;
+                    int counter = 0;
+                    int filasGridDatos = 1;
+                    // Read the file and display it line by line.  
+                    System.IO.StreamReader file =
+                        new System.IO.StreamReader(@""+ openFileDialog1.FileName);
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        counter++;
+                        if(counter<18)
+                        {
+
+                            switch (counter)
+                            {
+                                case 1:
+                                    break;
+
+                                case 2:
+                                    this.gridParametros[0, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+                                case 3:
+                                    this.gridParametros[counter-2, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+                                case 4:
+                                    this.gridParametros[counter - 2, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+                                case 5:
+                                    //counter-2 = 3
+                                    this.gridParametros[counter - 2, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+                                case 6:
+                                    //counter-2 = 4
+                                    this.gridParametros[counter - 2, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+                                case 7:
+                                    //counter-2 = 5
+                                    this.gridParametros[counter - 2, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+                                case 8:
+                                    //counter-2 = 6
+                                    this.gridParametros[counter - 2, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+                                case 9:
+                                    //counter-2 = 7
+                                    this.gridParametros[counter - 2, 0].Value = SacarTexto(line, 2);
+                                    break;
+
+
+                            }
+                        }
+                        else
+                        {
+                            if(counter>18 && counter<40)
+                            {
+
+                                this.gridDatos.Rows.Add(1);
+
+                                this.gridDatos[0, filasGridDatos-1].Value = ""+SacarTexto(line, 1);
+                                this.gridDatos[1, filasGridDatos-1].Value = "" + SacarTexto(line, 2);
+                                this.gridDatos[2, filasGridDatos - 1].Value = "" + SacarTexto(line, 3);
+                                this.gridDatos[3, filasGridDatos - 1].Value = "" + SacarTexto(line, 4);
+                                this.gridDatos[4, filasGridDatos - 1].Value = "" + SacarTexto(line, 5);
+                                this.gridDatos[5, filasGridDatos - 1].Value = "" + SacarTexto(line, 6);
+
+                                this.gridDatos.Refresh();
+                                filasGridDatos++;
+                            }
+
+                        }
+
+
+                    }
+                    counter = counter;
+                }
+            }
+            
+        }
+
+
+        public string SacarTexto(string linea,int ubicacion)
+        {
+            linea = linea.TrimEnd(',');
+            string[] arregloLinea = linea.Split(',');
+
+            return (arregloLinea.Length >= ubicacion) ? arregloLinea[ubicacion-1] : "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //GRID PARAMETROS COLUMNAS
+            gridParametros.Columns.Add("DeviceID", "Device ID"); //linea 2  del archivo
+            gridParametros.Columns.Add("SerialNumber", "Serial Number");  //linea 3 del archivo
+            gridParametros.Columns.Add("Kh", "Kh");  //linea 4  del archivo
+            gridParametros.Columns.Add("IntervalLength", "Interval Length");  //linea 5  del archivo
+            gridParametros.Columns.Add("TransformerFactor", "Transformer Factor");  //linea 6  del archivo
+            gridParametros.Columns.Add("TransformerFactor Applied", "Transformer Factor Applied");  //linea 7  del archivo
+            gridParametros.Columns.Add("MeterMultiplier", "Meter Multiplier");  //linea 8  del archivo
+            gridParametros.Columns.Add("MeterMultiplierApplied", "Meter Multiplier Applied");  //linea 9  del archivo
+            //////////////////////////////////////////////
+
+            //GRID DATOS COLUMNAS. LINEA 18 DEL ARCHIVO
+            gridDatos.Columns.Add("Time", "Time");
+            gridDatos.Columns.Add("Status", "Status");
+            gridDatos.Columns.Add("TotalkWh", "Total kWh");
+            gridDatos.Columns.Add("TotalkVARh", "TotalkVARh");
+            gridDatos.Columns.Add("TotalkW", "Total kW");
+            gridDatos.Columns.Add("DelkWh", "Del. kWh");
+
+           
+        }
+    }
+}
